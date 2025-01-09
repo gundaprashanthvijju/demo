0629da4e44d671b345ee904413042f9a2a2c573f
Prashanth <2211cs010673@mallareddyuniversity.ac.in>
2025-01-09 14:08:39 +0530

    js files

---
+++ day4/fun-expression.js
@@ -0,0 +1,48 @@
+//a =function ()
+//{
+    // return"gggg"
+//}
+//console.log(a);
+//console.log(a());
+
+//let main = function()
+//{
+//    let a = Number(prompt("enter a value"))
+//    let b = Number(prompt("enter b value"))
+//     let c = Number(prompt("enter c value"))
+//     let d = a+b+c
+//     console.log(d);
+//     let e = a-b-c
+//     console.log(e);
+//     let f= a*b*c
+//     console.log(f);
+//     if (a>=b && a>=c){
+//         console.log("a is gratest");
+//     }else if(b>=a &&  b>=c){
+//         console.log("b is gratest");
+//     }else if(c>=a && c>=b){
+//         console.log("c is gratest");
+
+//     }
+
+// }
+
+// main()
+// let b =funtion(a);{
+//     console.log(a);
+//     console.log(arguments[1]);
+//     console.log(arguments[2]);
+// }
+// b(10,1,2)
+
+function main(a){
+    console.log(a);
+
+}
+main (function(){
+    return "im vijju"
+})
+main (function(){
+    return "im prashanth"
+})
+
